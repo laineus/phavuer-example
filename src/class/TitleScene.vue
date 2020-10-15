f7b6e233f9729c60b8436183b1999f2c09a5b743
まふ <takiyama.tomomi@gmail.com>
2020-10-15 21:56:24 +0900

    title

--- src/class/TitleScene.vue
+++ src/class/TitleScene.vue
@@ -1,7 +1,6 @@
 <template>
   <Scene :ref="el => scene = el && el.scene" name="TitleScene" :autoStart="true">
-    <Rectangle :origin="0" :width="960" :height="540" fillColor="0x333333" :alpha="0.5" @pointerdown="onClick" />
-    <Text :origin="0.5" :x="480" :y="270">Title</Text>
+    <Sprite texture="title" :origin="0" @pointerdown="onClick" />
   </Scene>
 </template>
 
@@ -9,10 +8,9 @@
 import { ref } from 'vue'
 import { Repository } from './substanceUtils'
 import Scene from '../phavuer/components/Scene'
-import Text from '../phavuer/components/Text'
-import Rectangle from '../phavuer/components/Rectangle'
+import Sprite from '../phavuer/components/Sprite'
 export default {
-  components: { Scene, Rectangle, Text },
+  components: { Scene, Sprite },
   setup (props) {
     const scene = ref(null)
     const onClick = () => {
