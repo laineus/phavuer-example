ed0377223fd7062fdbbcaa6d9d407039cd213805
Laineus <lainen000@gmail.com>
2021-09-11 01:20:37 +0900

    assets loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import replace from '@rollup/plugin-replace'
+import phaserAssetsLoader from 'phaser-assets-loader'
 import packageJson from './package.json'
+
+const vitePhaserAssetsLoader = config => {
+  return {
+    name: 'phaser-assets-loader',
+    config (_, { mode }) {
+      const { exportJson, watch } = phaserAssetsLoader(config, { projectRoot: __dirname })
+      exportJson()
+      if (mode === 'development') {
+        watch()
+      }
+    }
+  }
+}
 
 export default defineConfig({
   plugins: [
     vue(),
+    vitePhaserAssetsLoader({
+      patterns: [
+        { type: 'image', prefix: '', dir: '/img', rule: /^\w+\.png$/ }
+      ],
+      output: 'src/assets.json'
+    }),
     replace({
       'APP_VERSION': JSON.stringify(packageJson.version),
       'typeof CANVAS_RENDERER': JSON.stringify(true),
