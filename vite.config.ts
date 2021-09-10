361e7f77b8eadf1e759af5b915ca04d904f7fb23
Laineus <lainen000@gmail.com>
2021-09-11 01:09:37 +0900

    replace plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import replace from '@rollup/plugin-replace'
+import packageJson from './package.json'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    replace({
+      'APP_VERSION': JSON.stringify(packageJson.version),
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true)
+    })
+  ]
 })
