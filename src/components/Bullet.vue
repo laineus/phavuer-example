75e0a35d8e3365ff0d6d4d55a0b393240c4734c1
Laineus <lainen000@gmail.com>
2021-09-15 22:42:16 +0900

    use v-model

--- src/components/Bullet.vue
+++ src/components/Bullet.vue
@@ -1,36 +1,35 @@
 <template>
-  <Image ref="object" texture="fire" :frame="frame" @create="create" />
+  <Image texture="fire" :frame="frame" v-model:x="x" v-model:y="y" :rotation="r" :blendMode="blendMode">
+    <Body :velocityX="velocityX" :velocityY="velocityY" />
+  </Image>
 </template>
 
 <script>
 import { inject, reactive, toRefs } from 'vue'
-import { refObj, Image, onPreUpdate } from 'phavuer'
+import { Image, Body, onPreUpdate } from 'phavuer'
 import { overScreen, closeTo, FrameAnimator } from './substanceUtils'
 export default {
-  components: { Image },
+  components: { Image, Body },
   props: ['init'],
   emits: ['destroy'],
   setup (props, context) {
-    const scene = inject('scene')
     const enemies = inject('enemies')
-    const object = refObj(null)
-    const data = reactive({ frame: 0, id: props.init.id, x: props.init.x, y: props.init.y, r: props.init.r })
+    const cos = Math.cos(props.init.r)
+    const sin = Math.sin(props.init.r)
+    const data = reactive({
+      frame: 0,
+      x: props.init.x + cos * 30,
+      y: props.init.y + sin * 30,
+      velocityX: cos * 300,
+      velocityY: sin * 300,
+      r: props.init.r
+    })
     const animator = new FrameAnimator([{ key: 'fire', frames: [0, 1, 2], duration: 5 }])
-    const create = object => {
-      const addX = Math.cos(data.r)
-      const addY = Math.sin(data.r)
-      object.setPosition(data.x + addX * 30, data.y + addY * 30)
-      object.setBlendMode(Phaser.BlendModes.OVERLAY)
-      object.setRotation(data.r)
-      scene.physics.world.enable(object)
-      object.body.setVelocity(addX, addY)
-      object.body.velocity.normalize().scale(300)
-    }
     onPreUpdate(() => {
       data.frame = animator.play('fire')
-      if (overScreen(object.value, 20)) context.emit('destroy')
+      if (overScreen(data, 20)) context.emit('destroy')
       enemies.value.some(enemy => {
-        if (enemy.ref.data.alive && closeTo(object.value, enemy.ref.object)) {
+        if (enemy.ref.data.alive && closeTo(data, enemy.ref.object)) {
           context.emit('destroy')
           enemy.ref.hit()
           return true
@@ -40,8 +39,7 @@ export default {
     })
     return {
       ...toRefs(data),
-      object,
-      create
+      blendMode: Phaser.BlendModes.OVERLAY
     }
   }
 }
