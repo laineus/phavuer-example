c09702aa426befd49bd8a3789e1061f8e54924be
Laineus <lainen000@gmail.com>
2021-09-16 01:07:28 +0900

    tween

--- src/components/Hit.vue
+++ src/components/Hit.vue
@@ -1,26 +1,20 @@
 <template>
-  <Image texture="hit" @create="create" />
+  <Image :tween="tween" />
 </template>
 
 <script>
-import { inject } from 'vue'
 import { Image } from 'phavuer'
 export default {
   components: { Image },
   emits: ['end'],
-  setup (props, context) {
-    const scene = inject('scene')
-    const create = object => {
-      object.setScale(0)
-      scene.add.tween({
-        targets: object,
-        scale: 1,
-        duration: 100,
-        onComplete: () => context.emit('end')
-      })
+  setup (_, context) {
+    const tween = {
+      scale: { from: 0, to: 1 },
+      duration: 100,
+      onComplete: () => context.emit('end')
     }
     return {
-      create
+      tween
     }
   }
 }
