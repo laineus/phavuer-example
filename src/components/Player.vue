00da8a8f4cbc5bf3710463645e48484429007bc8
Laineus <lainen000@gmail.com>
2021-09-16 00:26:19 +0900

    refactoring player

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,28 +1,43 @@
 <template>
-  <Container ref="object" @create="create" :depth="data.depth" v-model:x="data.x" v-model:y="data.y">
-    <Image ref="sprite" texture="spinel" :frame="data.frame" />
-    <Gauge :y="-30" :value="data.hp / maxHp" />
-    <Hit v-if="data.hitVisible" @end="data.hitVisible = false" :x="data.hitX" :y="data.hitY" />
+  <Container ref="object" :depth="y" v-model:x="x" v-model:y="y">
+    <Body :drag="300" :velocityX="velocityX" :velocityY="velocityY" />
+    <Image ref="sprite" texture="spinel" :frame="frame" />
+    <Gauge :y="-30" :value="hp / maxHp" />
+    <Hit v-if="hitVisible" @end="hitVisible = false" :x="hitX" :y="hitY" />
   </Container>
 </template>
 
 <script>
-import { inject, reactive } from 'vue'
-import { refObj, Container, Image, onPreUpdate } from 'phavuer'
+import { inject, reactive, toRefs } from 'vue'
+import { refObj, Container, Image, Body, onPreUpdate } from 'phavuer'
 import Gauge from './Gauge.vue'
 import Hit from './Hit.vue'
 import config from '../config'
 import { attack, dieAnimation, FrameAnimator, getAnimationKey8, WALK_ANIMATIONS_8 } from './substanceUtils'
 export default {
-  components: { Container, Image, Gauge, Hit },
+  components: { Container, Image, Body, Gauge, Hit },
   props: ['initialX', 'initialY'],
   emits: ['dead', 'shot'],
   setup (props, context) {
-    const scene = inject('scene')
     const object = refObj(null)
     const sprite = refObj(null)
     const tick = inject('tick')
-    const data = reactive({ hp: config.GAME.PLAYER_HP, x: props.initialX, y: props.initialY, lastDamaged: 0, frame: 0, flipX: false, depth: 0, tgtX: props.initialX, tgtY: props.initialY, hitVisible: false, hitX: 0, hitY: 0 })
+    const data = reactive({
+      hp: config.GAME.PLAYER_HP,
+      x: props.initialX,
+      y: props.initialY,
+      velocityX: 0,
+      velocityY: 0,
+      lastDamaged: 0,
+      frame: 0,
+      flipX: false,
+      depth: 0,
+      tgtX: props.initialX,
+      tgtY: props.initialY,
+      hitVisible: false,
+      hitX: 0,
+      hitY: 0
+    })
     const animator = new FrameAnimator(WALK_ANIMATIONS_8)
     const setTargetPosition = (x, y) => {
       data.tgtX = x
@@ -44,10 +59,6 @@ export default {
         context.emit('dead')
       })
     }
-    const create = object => {
-      scene.physics.world.enable(object)
-      object.body.setDrag(300)
-    }
     onPreUpdate(() => {
       data.depth = object.y
       const diffX = data.tgtX - object.value.x
@@ -66,10 +77,9 @@ export default {
       object.value.body.velocity.normalize().scale(200)
     })
     return {
+      ...toRefs(data),
       object,
       sprite,
-      data,
-      create,
       setTargetPosition,
       hit,
       maxHp: config.GAME.PLAYER_HP
