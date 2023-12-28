ee0c8f7e6b105cfc58fbb93c84dc7eaf751fee46
Laineus <lainen000@gmail.com>
2023-12-29 01:15:29 +0900

    update phavuer

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,35 +1,63 @@
 <template>
-  <TitleScene />
-  <GameScene @gameOver="onGameOver" />
-  <UIScene :result="result" @reset="onReset" />
+  <Game :config="gameConfig" @ready="onReady">
+    <TitleScene />
+    <GameScene @gameOver="onGameOver" />
+    <UIScene :result="result" @reset="onReset" />
+  </Game>
 </template>
 
 <script lang="ts">
 import { defineComponent, provide, ref } from 'vue'
-import { useGame } from 'phavuer'
+import { Game } from 'phavuer'
+import config from '../config'
 import TitleScene from './TitleScene.vue'
 import GameScene from './GameScene.vue'
 import UIScene from './UIScene.vue'
+const gameConfig = {
+  type: Phaser.AUTO,
+  width: config.WIDTH,
+  height: config.HEIGHT,
+  parent: 'game',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 }
+    }
+  },
+  audio: {
+    disableWebAudio: true
+  },
+  input: {
+    activePointers: 3
+  }
+}
 export default defineComponent({
-  components: { TitleScene, GameScene, UIScene },
+  components: { Game, TitleScene, GameScene, UIScene },
   setup () {
-    const game = useGame()
+    const game = ref<Phaser.Game>()
     const score = ref(0)
     const result = ref(false)
     const onGameOver = () => {
-      game.scene.pause('GameScene')
+      game.value?.scene.pause('GameScene')
       result.value = true
     }
     const onReset = () => {
-      game.scene.stop('GameScene')
-      game.scene.stop('UIScene')
-      game.scene.start('TitleScene')
+      game.value?.scene.stop('GameScene')
+      game.value?.scene.stop('UIScene')
+      game.value?.scene.start('TitleScene')
       result.value = false
     }
     provide('score', score)
+    const onReady = (v: Phaser.Game) => {
+      game.value = v
+      config.BLEND_MODES.OVERLAY = (v.renderer as Phaser.Renderer.WebGL.WebGLRenderer).addBlendMode([WebGLRenderingContext.SRC_ALPHA, WebGLRenderingContext.ONE], WebGLRenderingContext.FUNC_ADD)
+      window.addEventListener('resize', () => v.scale.refresh())
+    }
     return {
+      gameConfig,
       onGameOver,
       onReset,
+      onReady,
       result
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,5 @@
 import 'phaser'
 import { createApp } from 'vue'
-import config from './config'
-import { createPhavuerApp } from 'phavuer'
 import App from './components/App.vue'
 
-const game = new Phaser.Game({
-  type: Phaser.AUTO,
-  width: config.WIDTH,
-  height: config.HEIGHT,
-  parent: 'game',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 }
-    }
-  },
-  audio: {
-    disableWebAudio: true
-  },
-  input: {
-    activePointers: 3
-  }
-})
-config.BLEND_MODES.OVERLAY = (game.renderer as Phaser.Renderer.WebGL.WebGLRenderer).addBlendMode([WebGLRenderingContext.SRC_ALPHA, WebGLRenderingContext.ONE], WebGLRenderingContext.FUNC_ADD)
-const vueApp = createApp(App)
-createPhavuerApp(game, vueApp)
-window.addEventListener('resize', () => game.scale.refresh())
+const vueApp = createApp(App).mount('#game')
